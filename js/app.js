e150800af4ed964a031a3d772260600a47f90690
Arturo Monroy <arturomonroy1720@gmail.com>
2023-04-08 15:21:59 -0600

    Inicio

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ function iniciarApp() {
     selectCategorias.addEventListener('change', seleccionarCategoria)
 
     const resultado = document.querySelector('#resultado');
+    const  modal = new bootstrap.Modal('#modal', {});
 
     obtenerCategorias();
 
@@ -33,7 +34,14 @@ function iniciarApp() {
     }
 
     function mostrarRecetas(recetas = []) {
-        
+
+        limpiarHtml(resultado); 
+
+        const heading = document.createElement('H2');
+        heading.classList.add('text-center', 'text-black', 'my-5');
+        heading.textContent = recetas.length ? 'Resultados' : 'No hay Resultados';
+        resultado.appendChild(heading);
+
         //Iteramos en los resultados
         recetas.forEach( receta => {
             const { idMeal, strMeal, strMealThumb } = receta;
@@ -59,6 +67,12 @@ function iniciarApp() {
             const recetaButton = document.createElement('BUTTON');
             recetaButton.classList.add('btn', 'btn-danger', 'w-100');
             recetaButton.textContent = 'Ver Receta';
+            // recetaButton.dataset.bsTarget = '#modal';
+            // recetaButton.dataset.bsToggle = 'modal';
+            recetaButton.onclick = function() {
+                seleccionarReceta(idMeal);
+            }
+
 
             // Insertar en el HTML
             recetaCardBody.appendChild(recetaHeading);
@@ -71,7 +85,73 @@ function iniciarApp() {
 
             resultado.appendChild(recetaContenedor);
         })
+    }
+
+    function seleccionarReceta(id) {
+        const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetaModal(resultado.meals[0]))
+    }
+
+    function  mostrarRecetaModal(receta) {
         
+        const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+        console.log(receta)
+        // Añadiendo contenido al modal
+        const modalTitle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades</h3>
+        `;
+
+        const listGroup = document.createElement('UL');
+        listGroup.classList.add('list-group');
+        // Mostranto ingredientes + cantidades
+        for(let i = 1; i <= 20; i++) {
+            if(receta[`strIngredient${i}`]) {
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement('LI');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`
+
+                listGroup.appendChild(ingredienteLi);
+            }
+        }
+        modalBody.appendChild(listGroup);
+
+        const modalFooter = document.querySelector('.modal-footer');
+        limpiarHtml(modalFooter);
+        //Botones de cerrar y favorito
+        const btnFavorito = document.createElement('BUTTON');
+        btnFavorito.classList.add('btn', 'btn-danger', 'col');
+        btnFavorito.textContent = 'Guardar Favorito';
+
+        const btnCerrarModal = document.createElement('BUTTON');
+        btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
+        btnCerrarModal.textContent = 'Cerrar';
+        btnCerrarModal.onclick = function() {
+            modal.hide();
+        }
+
+        modalFooter.appendChild(btnFavorito);
+        modalFooter.appendChild(btnCerrarModal);
+
+        // Mostramos el modal
+        modal.show();
+    }
+
+    function limpiarHtml(selector) { 
+        while(selector.firstChild) {
+            resultado.removeChild(resultado.firstChild);
+        }
     }
 
 }
